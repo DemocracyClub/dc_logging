125839ff3274c56769706e30421d820070f2b3ad
GeoWill <roper.william@gmail.com>
2025-06-03 15:50:41 +0100

    BaseReportingStack which defines workgroup for reporting queries

---
+++ dc_logging_aws/models/buckets.py
@@ -0,0 +1,13 @@
+"""
+Define the S3 buckets used anywhere in the stack
+"""
+
+from models.models import S3Bucket
+
+postcode_searches_results_bucket = S3Bucket(
+    bucket_name="dc-monitoring-query-results"
+)
+
+postcode_searches_results_dev_bucket = S3Bucket(
+    bucket_name="dc-monitoring-dev-query-results"
+)
