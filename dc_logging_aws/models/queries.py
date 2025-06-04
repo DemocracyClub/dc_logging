72f341af11b3f8ca31833f228702b804938a62c9
GeoWill <roper.william@gmail.com>
2025-06-04 12:01:24 +0100

    Add query to get total postcode searches for reporting period

---
+++ dc_logging_aws/models/queries.py
@@ -0,0 +1,19 @@
+from models.databases import dc_wide_logs_db
+from models.models import BaseQuery
+
+total_searches_query = BaseQuery(
+    name="total_searches_query",
+    creation_context={
+        "query_file_path": "election_reporting/totals/total_searches_query.sql"
+    },
+    database=dc_wide_logs_db,
+    query_context={
+        "start_of_election_period_day": "",
+        "polling_day": "",
+        "updown_api_key": "",
+        "start_datetime_utc": "",
+        "end_datetime_utc": "",
+        "start_datetime_london": "",
+        "end_datetime_london": "",
+    },
+)
