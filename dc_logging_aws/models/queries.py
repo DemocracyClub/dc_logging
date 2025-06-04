6b016bba535e03db402786fd6a9874c4fe071527
GeoWill <roper.william@gmail.com>
2025-06-04 17:09:20 +0100

    Add by product query

--- dc_logging_aws/models/queries.py
+++ dc_logging_aws/models/queries.py
@@ -34,3 +34,20 @@ by_local_authority_query = BaseQuery(
         "end_datetime_london": "",
     },
 )
+
+by_product_query = BaseQuery(
+    name="by_product_query",
+    creation_context={
+        "query_file_path": "election_reporting/searches_by_product.sql"
+    },
+    database=dc_wide_logs_db,
+    query_context={
+        "start_of_election_period_day": "",
+        "polling_day": "",
+        "updown_api_key": "",
+        "start_datetime_utc": "",
+        "end_datetime_utc": "",
+        "start_datetime_london": "",
+        "end_datetime_london": "",
+    },
+)
